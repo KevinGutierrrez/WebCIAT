9659b22fedf05448b4496998bc2a3d39c8ec36ed
LAPTOP-Q4CIISV5\Kevin Gutierrez <kevin9704@hotmail.com>
2018-05-28 09:41:38 -0500

    creacion de usuarios y "preservar informacion"
    crea de manera dinamica, preservar informacion en txt,

---
+++ JS/LeerArchivo.js
@@ -0,0 +1,62 @@
+// Variable que contiene lo que lee del archivo "file.txt"
+let dataFile = "nada";
+
+//Se ejecuta cuando ya cargó toda la página
+$(function () {
+    console.log("ready!");
+    traerArchivo();
+});
+
+//Funcion recursiva que se actualiza cada 500 milisegundos
+function traerArchivo() {
+    setTimeout(function () {
+        ajax();
+        var contenidoArchivo = dataFile;
+        var partir = contenidoArchivo.split(",");
+        
+        for (let  i=0;  i < partir.length; i=i+6) {
+            var o=i+1;
+            var nombreUsuario = partir[i];
+            var apellidoUsuario = partir[o];
+            var nombreCompleto = nombreUsuario+" "+apellidoUsuario;
+            console.log(partir[i]);
+            var numerodeusuarios = partir.length/6;
+            if (numerodeusuarios>1) {
+
+                $("ol").remove("li");
+               
+            } else {
+                $("ol").append("<li><div class='UserInterface'><div class='containerUserInterface'><div class='containerName'>" + nombreCompleto + "</div><div class='containerButtonEdit'><button id='ButtonEditUser' class='btn btn-warning'><span class='glyphicon glyphicon-pencil' id='PencilIcon'></span>Edit</button></div></div></div></li>");
+            }        
+        }
+        
+        traerArchivo();
+    }, 1000);
+}
+
+//Comuninación con el script de php
+function ajax() {
+    $.ajax({
+        // la URL para la petición
+        url: './php/leerArchivo.php',
+
+        // especifica si será una petición POST o GET
+        type: 'POST',
+
+        // código a ejecutar si la petición es satisfactoria;
+        // la respuesta es pasada como argumento a la función
+        success: function (data) {
+            // console.log(data);
+            dataFile = data;
+        },
+
+        // código a ejecutar si la petición falla;
+        // son pasados como argumentos a la función
+        // el objeto de la petición en crudo y código de estatus de la petición
+        error: function (xhr, status) {
+            console.log("Error");
+            console.log(xhr);
+            console.log(status);
+        },
+    });
+}
---
+++ PHP/leerArchivo.php
@@ -0,0 +1,6 @@
+<?php
+    $ubicacion = "../Lista de usuarios.txt";
+    $myfile = fopen($ubicacion, "r") or die("No se puede abrir el archivo!");
+    echo fread($myfile,filesize($ubicacion));
+    fclose($myfile);
+?>
